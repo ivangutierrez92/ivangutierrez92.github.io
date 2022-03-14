c81ca5edb3303b03483904ecf540e3cd70276f3b
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-03-14 18:06:11 -0300

    Added favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,13 +2,21 @@ import '../styles/globals.css';
 import Layout from '../components/Layout';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import Head from 'next/head';
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
