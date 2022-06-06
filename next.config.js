939bfd36da8693df85908b4577a79f80155e37c8
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-06-06 15:55:55 -0400

    Fixed image support

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compress: true
+  compress: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  productionBrowserSourceMaps: true,
 }
 
 module.exports = nextConfig
