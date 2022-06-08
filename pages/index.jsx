92561c7e7af7cd1895a07126d127c9c6e801d688
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-06-08 10:50:21 -0400

    fixed banner space

--- pages/index.jsx
+++ pages/index.jsx
@@ -51,6 +51,7 @@ export default function Home() {
             width={1000}
             height={1000}
             layout="responsive"
+            priority
           />
         </div>
       </div>
